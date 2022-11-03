b64cdb5dd83746bcf8357486f6b4fc3face377c2
naohCodeS <naoh.suzuki@icloud.com>
2022-11-03 19:53:19 +0900

    バックエンド

--- tic-tac-toe/src/main/java/com/javamaster/tictactoe/configuration/WebSocketConfiguration.java
+++ tic-tac-toe/src/main/java/com/javamaster/tictactoe/configuration/WebSocketConfiguration.java
@@ -13,7 +13,6 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/gameplay").withSockJS();
     }
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
